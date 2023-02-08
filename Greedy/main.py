6013b4e2b6fb062da0a690625ce037c3a1087a03
ktyoung <ktyoung0507@gmail.com>
2023-02-08 22:52:18 +0900

    그리디(큰 수의 법칙)

--- Greedy/main.py
+++ Greedy/main.py
@@ -10,3 +10,26 @@ for coin in coin_types:
     n %= coin
 
 print(count)
+
+# 문제 1. 큰 수의 법칙
+n, m, k = map(int, input().split())     # N, M, K 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))  # N개의 수를 공백으로 구분하여 입력받기
+
+data.sort()             # 입력받은 수 정렬
+first = data[n - 1]     # 가장 큰 수
+second = data[n - 2]    # 다음으로 큰 수
+
+result = 0
+
+while True:
+    for i in range(k):  # 가장 큰 수를 k번 더하기
+        if m == 0:      # m == 0이면 반복문 탈출
+            break
+        result += first
+        m -= 1          # 덧셈을 수행할 때마다 m을 1씩 감소
+    if m == 0:
+        break
+    result += second    # 두 번째로 큰 수를 한 번 더하기
+    m -= 1              # 덧셈을 수행할 때마다 m을 1씩 감소
+
+print(result)           # 최종 답안 출력
