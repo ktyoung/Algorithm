660e4f0f71ead260569b1024e03057c6e5168596
ktyoung <ktyoung0507@gmail.com>
2023-02-10 23:33:34 +0900

    그리디(1이 될 때까지)

--- Greedy/main.py
+++ Greedy/main.py
@@ -45,3 +45,20 @@ for i in range(n):
     result = max(result, min_value)
 
 print(result)
+
+# 문제 3. 1이 될 때까지
+n, k = map(int, input().split())
+result = 0
+
+while n >= k:
+    while n % k != 0:
+        n -= 1
+        result += 1
+    n //= k
+    result += 1
+
+while n > 1:
+    n -= 1
+    result += 1
+
+print(result)
