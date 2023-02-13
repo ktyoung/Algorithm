b0d6fc97065128b646206cce37f1d028a9de8823
ktyoung <ktyoung0507@gmail.com>
2023-02-13 21:53:16 +0900

    구현(시각)

--- Implementation/main.py
+++ Implementation/main.py
@@ -18,3 +18,15 @@ for plan in plans:
     x, y = nx, ny
 
 print(x, y)
+
+# 예제 4-2. 시각
+h = int(input())
+
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
