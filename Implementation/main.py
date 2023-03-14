dbc5fda76a44c4e93b16c0d6825c052ae3e31084
ktyoung <ktyoung0507@gmail.com>
2023-03-14 21:31:09 +0900

    구현(게임 개발)

--- Implementation/main.py
+++ Implementation/main.py
@@ -31,7 +31,7 @@ for i in range(h + 1):
 
 print(count)
 
-# 실전문제 2. 왕실의 나이트
+# 예제 4-3. 왕실의 나이트
 input_data = input()
 row = int(input_data[1])
 column = int(ord(input_data[0])) - int(ord('a')) + 1
@@ -47,3 +47,55 @@ for step in steps :
         result += 1
 
 print(result)
+
+# 예제 4-4. 게임 개발
+n, m = map(int, input().split())
+
+d = [[0] * m for _ in range(n)]
+x, y, direction = map(int, input().split())
+d[x][y] = 1
+
+array = []
+for i in range(n) :
+    array.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left() :
+    global direction
+    direction -= 1
+    if direction == -1 :
+        direction = 3
+
+count = 1
+turn_time = 0
+while True :
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if d[nx][ny] == 0 and array[nx][ny] == 0 :
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+
+    else :
+        turn_time += 1
+
+    if turn_time == 4 :
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+
+        if array[nx][ny] == 0 :
+            x = nx
+            y = ny
+
+        else :
+            break
+        turn_time = 0
+
+print(count)
