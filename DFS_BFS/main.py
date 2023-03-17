5937f6367b8191b60d8f7e52dd75c4ab17e9be5f
ktyoung <ktyoung0507@gmail.com>
2023-03-17 20:41:24 +0900

    DFS/BFS(재귀 함수)

--- DFS_BFS/main.py
+++ DFS_BFS/main.py
@@ -31,3 +31,36 @@ queue.popleft()
 print(queue)
 queue.reverse()
 print(queue)
+
+# 5-3. 재귀 함수 예제
+def recursive_function() :
+    print("재귀 함수를 호출합니다.")
+    recursive_function()
+
+recursive_function()
+
+# 5-4. 재귀 함수 종료 예제
+def recursive_function(i) :
+    if i == 100:
+        return
+    print(i, "번째 재귀 함수에서", i + 1, "번째 재귀 함수를 호출합니다.")
+    recursive_function(i+1)
+    print(i, "번째 재귀 함수를 종료합니다.")
+
+recursive_function(1)
+
+# 5-5. 2가지 방식으로 구현한 팩토리얼 예제
+def factorial_interative(n) :
+    result = 1
+    for i in range(1, n + 1) :
+        result *= i
+    return result
+
+def factorial_recursive(n) :
+    if n <= 1 :
+        return 1
+
+    return n * factorial_recursive(n - 1)
+
+print("반복적으로 구현:", factorial_interative(5))
+print("재귀적으로 구현:", factorial_recursive(5))
