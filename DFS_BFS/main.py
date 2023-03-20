ed14a02bfae623d0ac5c53b00a83ef8517c361db
ktyoung <ktyoung0507@gmail.com>
2023-03-20 21:04:17 +0900

    DFS/BFS(DFS)

--- DFS_BFS/main.py
+++ DFS_BFS/main.py
@@ -64,3 +64,51 @@ def factorial_recursive(n) :
 
 print("반복적으로 구현:", factorial_interative(5))
 print("재귀적으로 구현:", factorial_recursive(5))
+
+# 5-6. 인접 행렬 방식 예제
+INF = 99999999
+
+graph = [
+    [0, 7, 5],
+    [7, 0, INF],
+    [5, INF, 0]
+]
+
+print(graph)
+
+# 5-7. 인접 리스트 방식 예제
+graph = [[] for _ in range(3)]
+
+graph[0].append((1, 7))
+graph[0].append((2, 5))
+
+graph[1].append((0, 7))
+
+graph[2].append((0, 5))
+
+print(graph)
+
+# 5-8. DFS 예제
+def dfs(graph, v, visited) :
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v] :
+        if not visited[i] :
+            dfs(graph, i, visited)
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
