c03cb8f59c66cb4362bd1dce81c69ee760b56d08
ktyoung <ktyoung0507@gmail.com>
2023-03-28 21:17:14 +0900

    정렬(퀵 정렬_파이썬)

--- Sorting/main.py
+++ Sorting/main.py
@@ -49,3 +49,20 @@ def quick_sort(array, start, end) :
 
 quick_sort(array, 0, len(array) - 1)
 print(array)
+
+# 6-5. 퀵 정렬 소스코드(파이썬)
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array) :
+    if len(array) <= 1 :
+        return array
+
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
