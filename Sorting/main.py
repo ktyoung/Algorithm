fa63c31ed1ea94f666992da7bb536753e62a0707
ktyoung <ktyoung0507@gmail.com>
2023-03-30 20:27:13 +0900

    정렬(정렬 라이브러리)

--- Sorting/main.py
+++ Sorting/main.py
@@ -78,3 +78,24 @@ for i in range(len(array)) :
 for i in range(len(count)) :
     for j in range(count[i]) :
         print(i, end=" ")
+
+# 6-7. sorted 소스 코드
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+result = sorted(array)
+print(result)
+
+# 6-8. sort 소스코드
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+array.sort()
+print(array)
+
+# 6-9. 정렬 라이브러리에서 key를 활용한 소스코드
+array = [('바나나', 2), ('사과', 5), ('당근', 3)]
+
+def setting(data) :
+    return data[1]
+
+result = sorted(array, key=setting)
+print(result)
