d363c9f6804606dd9cd0e350faae88dd05bee52d
ktyoung <ktyoung0507@gmail.com>
2023-04-13 18:55:39 +0900

    다이나믹 프로그래밍(피보나치 수열)

--- DynamicProgramming/main.py
+++ DynamicProgramming/main.py
@@ -20,3 +20,29 @@ def fibo(x):
     return d[x]
 
 print(fibo(99))
+
+# 8-3. 호출되는 함수 확인
+d = [0] * 100
+
+def pibo(x):
+    print('f(' + str(x) + ')', end=' ')
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = pibo(x - 1) + pibo(x - 2)
+    return d[x]
+
+pibo(6)
+
+# 6-4. 피보나치 수열 소스코드(반복적)
+d = [0] * 100
+
+d[1] = 1
+d[2] = 1
+n = 99
+
+for i in range(3, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
+
+print(d[n])
