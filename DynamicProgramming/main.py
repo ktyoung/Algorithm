2385f47fc3685d192bb60f9a849aeb7f43aed9bf
ktyoung <ktyoung0507@gmail.com>
2023-04-17 20:16:57 +0900

    다이나믹 프로그래밍(개미 전사)

--- DynamicProgramming/main.py
+++ DynamicProgramming/main.py
@@ -63,3 +63,16 @@ for i in range(2, x + 1):
         d[i] = min(d[i], d[i // 5] + 1)
 
 print(d[x])
+
+# 실전 문제 3. 개미 전사
+n = int(input())
+array = list(map(int, input().split()))
+
+d = [0] * 100
+
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + array[i])
+
+print(d[n - 1])
