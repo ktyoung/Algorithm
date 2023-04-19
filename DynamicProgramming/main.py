19de0ead6fe7127a95347216a2d7f2803dbf6408
ktyoung <ktyoung0507@gmail.com>
2023-04-19 20:41:18 +0900

    다이나믹 프로그래밍(효율적인 화폐 구성)

--- DynamicProgramming/main.py
+++ DynamicProgramming/main.py
@@ -88,3 +88,23 @@ for i in range(3, n + 1):
     d[i] = (d[i - 1] + 2 * d[i - 2]) % 796796
 
 print(d[n])
+
+# 실전 문제 5. 효율적인 화폐 구성
+n, m = map(int, input().split())
+array = []
+
+for i in range(n):
+    array.append(int(input()))
+
+d = [10001] * (m + 1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001:
+            d[j] = min(d[j], d[j - array[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
