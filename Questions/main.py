eeaee4556285d70fec0fa63f0ecd8bf69cafeda2
ktyoung <ktyoung0507@gmail.com>
2023-05-10 20:45:33 +0900

    그리디 문제(곱하기 혹은 더하기)

--- Questions/main.py
+++ Questions/main.py
@@ -14,3 +14,17 @@ for i in data:
         count = 0
 
 print(result)
+
+## 02. 곱하기 혹은 더하기
+data = input()
+result = int(data[0])
+
+for i in range(1, len(data)):
+    num = int(data[i])
+
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+
+print(result)
