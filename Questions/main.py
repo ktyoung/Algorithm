e958b13d886d9588c01d5b915124b32930ada187
ktyoung <ktyoung0507@gmail.com>
2023-05-18 21:00:25 +0900

    구현 문제(문자열 재정렬)

--- Questions/main.py
+++ Questions/main.py
@@ -117,3 +117,21 @@ if summary == 0:
     print("LUCKY")
 else:
     print("READY")
+
+## 02. 문자열 재정렬
+data = input()
+result = []
+value = 0
+
+for x in data:
+    if x.isalpha():
+        result.append(x)
+    else:
+        value += int(x)
+
+result.sort()
+
+if value != 0:
+    result.append(str(value))
+
+print(''.join(result))
