e862831dfe4578cd362b53970986f2eee7bffff3
ktyoung <ktyoung0507@gmail.com>
2023-05-20 13:23:22 +0900

    구현 문제(문자열 압축)

--- Questions/main.py
+++ Questions/main.py
@@ -135,3 +135,25 @@ if value != 0:
     result.append(str(value))
 
 print(''.join(result))
+
+## 03. 문자열 압축
+def solution(s):
+    answer = len(s)
+
+    for step in range(1, len(s) // 2 + 1):
+        compressed = ""
+        prev = s[0:step]
+        count = 1
+
+        for j in range(step, len(s), step):
+            if prev == s[j:j + step]:
+                count += 1
+            else:
+                compressed += str(count) + prev if count >= 2 else prev
+                prev = s[j:j + step]
+                count = 1
+
+        compressed += str(count) + prev if count >= 2 else prev
+        answer = min(answer, len(compressed))
+
+    return answer
