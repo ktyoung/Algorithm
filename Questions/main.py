cb3fc3eee98a23414062650c45ab13fe20ad73a5
ktyoung <ktyoung0507@gmail.com>
2023-06-15 20:21:34 +0900

    다이나믹 프로그래밍 문제(금광)

--- Questions/main.py
+++ Questions/main.py
@@ -913,3 +913,36 @@ def solution(words, queries):
             res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
         answer.append(res)
     return answer
+
+# 5. 다이나믹 프로그래밍
+## 01. 금광
+for tc in range(int(input())):
+    n, m = map(int, input().split())
+    array = list(map(int, input().split()))
+
+    dp = []
+    index = 0
+    for i in range(n):
+        dp.append(array[index:index + m])
+        index += m
+
+    for j in range(1, m):
+        for i in range(n):
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i - 1][j - 1]
+
+            if i == n - 1:
+                left_down = 0
+            else:
+                left_down = dp[i + 1][j - 1]
+
+            left = dp[i][j - 1]
+            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
+
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m - 1])
+
+    print(result)
