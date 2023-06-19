d45a24e7a30ffc57a86f3f42860487b531df242a
ktyoung <ktyoung0507@gmail.com>
2023-06-19 20:28:38 +0900

    다이나믹 프로그래밍 문제(퇴사)

--- Questions/main.py
+++ Questions/main.py
@@ -968,3 +968,25 @@ for i in range(1, n):
         dp[i][j] = dp[i][j] + max(up_left, up)
 
 print(max(dp[n - 1]))
+
+## 03. 퇴사
+n = int(input())
+t = []
+p = []
+dp = [0] * (n + 1)
+max_value = 0
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    t.append(x)
+    p.append(y)
+
+for i in range(n - 1, -1, -1):
+    time = t[i] + i
+    if time <= n:
+        dp[i] = max(p[i] + dp[time], max_value)
+        max_value = dp[i]
+    else:
+        dp[i] = max_value
+
+print(max_value)
