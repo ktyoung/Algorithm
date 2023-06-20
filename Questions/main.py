6f05449433277e00c45bc4aeda38c2d349983f97
Kim TaeYoung <108244404+ktyoung@users.noreply.github.com>
2023-06-20 12:14:59 +0900

    다이나믹 프로그래밍 문제(병사 배치하기)

--- Questions/main.py
+++ Questions/main.py
@@ -989,4 +989,17 @@ for i in range(n - 1, -1, -1):
     else:
         dp[i] = max_value
 
+## 04. 병사 배치하기
+n = int(input())
+array = list(map(int, input().split()))
+array.reverse()
+
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
 print(max_value)
